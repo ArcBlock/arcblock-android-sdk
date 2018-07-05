5480a1130dcfe65ba3727e352bbeeaf151acc444
NateRobinson <840501291@qq.com>
2018-07-05 23:35:15 +0800

    change activity to fragment for reuse and manage.

---
+++ app/src/main/java/com/arcblock/sdk/demo/CoreKitFragment.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017-present ArcBlock Foundation Ltd <https://www.arcblock.io/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.arcblock.sdk.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arcblock.sdk.demo.corekit.QueryListBlocksActivity;
+
+public class CoreKitFragment extends Fragment {
+
+	public static CoreKitFragment newInstance() {
+		CoreKitFragment fragment = new CoreKitFragment();
+		return fragment;
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.frag_corekit, null);
+		return view;
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		view.findViewById(R.id.query_list_blocks_data_btn).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), QueryListBlocksActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+	}
+
+}
---
+++ app/src/main/java/com/arcblock/sdk/demo/adapter/MainAdapter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017-present ArcBlock Foundation Ltd <https://www.arcblock.io/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.arcblock.sdk.demo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class MainAdapter extends FragmentStatePagerAdapter {
+	private List<Fragment> mFragments;
+	private List<String> mTitles;
+
+	public MainAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+		super(fm);
+		mFragments = fragments;
+		mTitles = titles;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		return mFragments.get(position);
+	}
+
+	@Override
+	public int getCount() {
+		return mFragments == null ? 0 : mFragments.size();
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return mTitles.get(position);
+	}
+}
